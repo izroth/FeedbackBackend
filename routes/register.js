6ef61c2434405c7bfa7b5cbf696502885606f37b
izroth <lakhansharma889027@gmail.com>
2023-08-25 19:21:01 +0530

    changes made

--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const app = express()
 const router = express.Router()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
 const registercontroller = require('../Controllers/Register/register')
+const logincontroller = require('../Controllers/Register/login')
+
 
 router.post('/', registercontroller)
+router.post('/login', logincontroller)
 module.exports = router
